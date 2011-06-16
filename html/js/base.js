34203b9853b3f4aca14bbfc63505545a82542759
Matej <mkram0@hotmail.co.uk>
2011-06-16 13:52:36 +0100

    JavaScript animates whole navigation bar. Custom JQuery plugin was added to not waste kb of space in the JavaScript document.

--- html/js/base.js
+++ html/js/base.js
@@ -1,12 +1,42 @@
 $(document).ready(function(){
-    $(".JQ_about_me a").mouseover(function(){
-        $(".JQ_about_me div#about_me, .JQ_about_me div#about_me_active").stop(true).animate({ 'bottom':'50px' }, 'fast');
-        $(".JQ_about_me div#about_me_active").css ( { 'opacity': 0, 'visibility': 'visible' } ).animate( { opacity: 1 }, 'fast' );
-        $(this).animate ( { 'top':'-50px', 'height':'129px' } );
+    $(".JQ_about_me").nav_flyup( { div:"about_me", baseDiv:".JQ_about_me" } );
+    $(".JQ_log_in").nav_flyup( { div:"log_in", baseDiv:".JQ_log_in" } );
+    $(".JQ_portfolio").nav_flyup( { div:"portfolio", baseDiv:".JQ_portfolio" } );
+    $(".JQ_contact").nav_flyup( { div:"contact", baseDiv:".JQ_contact" } );
+});
+
+$.fn.nav_flyup = function (){
+    var args = arguments[0] || {};
+    
+    var nav = args.div;
+    var base = args.baseDiv;
+    
+    $(base + " a").mouseover(function(){
+        $(base + " div#" + nav + ", " + base + " div#" + nav + "_active").stop(true).animate({
+            'bottom':'50px'
+        }, 'fast');
+        $(base + " div#" + nav + "_active").css ( {
+            'opacity': 0, 
+            'visibility': 'visible'
+        } ).animate( {
+            opacity: 1
+        }, 'fast' );
+        $(base + " a").animate ( {
+            'top':'-50px', 
+            'height':'129px'
+        } );
     }).mouseout (function(){
-        $(".JQ_about_me div#about_me_active").stop(true).animate( { opacity: 0 }, 'fast', function() {
-            $(".JQ_about_me a").animate( { 'top':'0px', 'height':'79px' } );
-            $(".JQ_about_me div#about_me").animate({ 'height':'79px', 'bottom':'0px'  }, 'slow');
+        $(base + " div#" + nav + "_active").stop(true).animate( {
+            opacity: 0
+        }, 'fast', function() {
+            $(base + " a").animate( {
+                'top':'0px', 
+                'height':'79px'
+            } );
+            $(base + " div#" + nav).animate({
+                'height':'79px', 
+                'bottom':'0px'
+            }, 'slow');
         } );
     });
-});
+};
